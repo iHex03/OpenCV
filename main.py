25e83c567749baed206e12e70cd3f4f49522fda0
iHex03 <lecrammattos@live.com>
2021-04-06 16:14:08 -0300

    Adicionando Main, IMG e Video inicial

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+# img = cv.imread('Fotos/Capturar.png')
+# cv.imshow('Fodase', img)
+
+# cv.waitKey(0)
+
+
+#0, 1, 2, [...] para webcam, para vídeo apenas botar diretório
+video = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = video.read()
+    cv.imshow('video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
